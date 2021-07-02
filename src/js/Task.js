b4623ba222b7ebda6e74ebfd6d6d62797e277140
lthonny <thonny02@mail.ru>
2021-07-02 21:25:56 +0300

    error style js checkox

--- src/js/Task.js
+++ src/js/Task.js
@@ -5,3 +5,20 @@ function Task(text, completed, id, date) {
 
   this.date = date;
 }
+
+
+function* Random(seed) {
+  let value = seed;
+
+  while (true) {
+    value = value * 16213 % 232013201312
+    yield value;
+  }
+};
+
+
+let generator = Random(1);
+
+console.log(generator.next().value)
+console.log(generator.next().value)
+console.log(generator.next().value)
